0ae16d7c1267b5d77c8909e6027417c9ac561cb1
Gustavo Kikey <you@example.com>
2023-03-11 10:51:40 -0400

    initial commit

---
+++ scripts.js
@@ -0,0 +1,58 @@
+const timer = {
+  pomodoroTime: 2,
+  breakTime: 1,
+  teste: 10,
+};
+
+let interval;
+
+document.addEventListener("DOMContentLoaded", () => {
+  resetMode("pomodoroTime");
+});
+
+const pomodorobutton = document.getElementById("pomodoro-btn");
+pomodorobutton.addEventListener("click", () => {
+  resetMode("pomodoroTime");
+  startTimer();
+});
+
+const shortbreakbutton = document.getElementById("break-btn");
+shortbreakbutton.addEventListener("click", () => {
+  resetMode("breakTime");
+  startTimer();
+});
+
+function startTimer() {
+  interval = setInterval(updateClock, 1000);
+}
+
+function resetMode(mode) {
+  clearInterval(interval);
+  timer.timeRemaining = {
+    total: timer[mode] * 60,
+    minutes: timer[mode],
+    seconds: 0,
+  };
+  drawClock();
+}
+
+function updateClock() {
+  if (timer.timeRemaining.total === 0) {
+    clearInterval(interval);
+  } else if (timer.timeRemaining.seconds === 0) {
+    timer.timeRemaining.minutes--;
+    timer.timeRemaining.seconds = 59;
+  } else {
+    timer.timeRemaining.seconds--;
+  }
+  timer.timeRemaining.total--;
+  drawClock();
+}
+
+function drawClock() {
+  let minutes = `${timer.timeRemaining.minutes}`.padStart(2, "0");
+  let seconds = `${timer.timeRemaining.seconds}`.padStart(2, "0");
+
+  document.getElementById("minute").innerHTML = minutes;
+  document.getElementById("second").innerHTML = seconds;
+}
